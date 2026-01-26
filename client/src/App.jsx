1004b0f5788310b4fda48ce5655ae6d0127f1162
Kyle <caragk24@gmail.com>
2026-01-26 22:20:42 +0800

    Testing User Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter as Router } from 'react-router-dom'; // 👈 ADD THIS
 import Login from './pages/Login'; 
 import Dashboard from './pages/AdminDashboard'; 
-import ItemGallery from './pages/ItemGallery'; // Import the new file
+import ItemGallery from './pages/ItemGallery'; // 👈 Ensure this matches your filename
 
 function App() {
   const [user, setUser] = useState(null);
@@ -36,7 +36,7 @@ function App() {
   if (loading) return <div style={{color:'white', textAlign:'center', marginTop:'20%'}}>Checking Session...</div>;
 
   return (
-    <GoogleOAuthProvider clientId="YOUR_CLIENT_ID">
+    <GoogleOAuthProvider clientId="1002059220341-9vj4rqbb1p9808ludct00s0cc2oi5734.apps.googleusercontent.com">
       <Router>
         <div className="app">
           {!user ? (
